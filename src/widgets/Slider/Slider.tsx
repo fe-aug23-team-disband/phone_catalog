df2dd0e76bee80283a10a9fd65b4b7686c0e975a
Illia Drach <illyadrach@gmail.com>
2023-12-12 19:02:29 +0200

    Slider-Problem-(DO NOT MERGE)

--- src/widgets/Slider/Slider.tsx
+++ src/widgets/Slider/Slider.tsx
@@ -1,19 +1,30 @@
-// import SliderTool from "react-slick";
-// import phones from "./phones.json";
-// import { ItemCard } from "../../entities/ItemCard/ItemCard";
+import SliderTool from "react-slick";
+import phones from "./phones.json";
+import styles from "./Slider.module.scss";
+import { ItemCard } from "../../entities/ItemCard/ItemCard";
 
 export const Slider = () => {
-  // const settings = {
-  //   dots: true,
-  //   infinite: false,
-  //   rows: 1,
-  //   speed: 500,
-  //   slidesPerRow: 4,
-  //   slidesToScroll: 4,
+  const firstEight = [phones[0], phones[1], phones[2], phones[3], phones[4], phones[5], phones[6], phones[7]];
 
-  // };
+  const settings = {
+    dots: true,
+    infinite: false,
+    rows: 4,
+    speed: 500,
+    slidesPerRow: 4,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+  };
 
   return (
-    <h1>Slider</h1>
+    <div className={styles.SliderStyle}>
+      <SliderTool {...settings}>
+        {firstEight.map(phone => (
+          <div className="" key={phone.id}>
+            <ItemCard phone={phone} />
+          </div>
+        ))}
+      </SliderTool>
+    </div>
   );
 };
