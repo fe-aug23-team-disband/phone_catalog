185124e6c6200f9147ed25d17a2c2a5f744773a4
Serhii Mykhailivskyi <sergeymih098123@gmail.com>
2023-12-13 00:00:46 +0200

    s

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,18 @@ import { Outlet } from "react-router-dom";
 import Header from "../widgets/Header/Header";
 import Footer from "../widgets/Footer/Footer";
 import styles from "../styles/main.module.scss";
+import { ItemCard } from "../entities/ItemCard/ItemCard";
+import phones from "../widgets/Slider/phones.json";
 
 export const App = () => {
+  const phone = phones[0];
+
   return (
     <div className={styles.appWrapper}>
       <Header />
       <main id={"top"} className={styles.mainWrapper}>
         <div className={styles.mainContent}>
+          <ItemCard phone={phone} />
           <Outlet />
         </div>
       </main>
