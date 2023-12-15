8faacf5666799229849c434ba9338ca15a586a55
Alex <opetrovskyi423@gmail.com>
2023-12-15 23:49:16 +0100

    fix_main_card_slider

--- src/widgets/ItemSlider/ItemSlider.tsx
+++ src/widgets/ItemSlider/ItemSlider.tsx
@@ -9,113 +9,114 @@ import "slick-carousel/slick/slick-theme.css";
 import styles from "./ItemSlider.module.scss";
 import "./ItemSlider.scss";
 import { ItemCard } from "../../entities/ItemCard/ItemCard";
-import {useAsyncValue} from "react-router";
-import {ProductShorted} from "../../types/Product";
-import classNames from "classnames";
+import { useAsyncValue } from "react-router";
+import { ProductShorted } from "../../types/Product";
+// import classNames from "classnames";
 interface Props {
   name: string;
   data?: PhoneType;
 }
 
-interface ArrowProps {
-  onClick?: () => void;
-  isDisabled: boolean;
-}
+// interface ArrowProps {
+//   onClick?: () => void;
+//   isDisabled: boolean;
+// }
 
-function SamplePrevArrow(props: ArrowProps) {
-  const { onClick, isDisabled } = props;
-  const classes = classNames("arrow arrow-left", { disabled: isDisabled });
+// function SamplePrevArrow(props: ArrowProps) {
+//   const { onClick, isDisabled } = props;
+//   const classes = classNames("arrow arrow-left", { disabled: isDisabled });
 
-  return (
-    <div onClick={isDisabled ? undefined : onClick} className={classes} />
-  );
-}
+//   return (
+//     <div onClick={isDisabled ? undefined : onClick} className={classes} />
+//   );
+// }
 
-function SampleNextArrow(props: ArrowProps) {
-  const { onClick, isDisabled } = props;
-  const classes = classNames("arrow arrow-right", { disabled: isDisabled });
+// function SampleNextArrow(props: ArrowProps) {
+//   const { onClick, isDisabled } = props;
+//   const classes = classNames("arrow arrow-right", { disabled: isDisabled });
 
-  return (
-    <div onClick={isDisabled ? undefined : onClick} className={classes} />
-  );
-}
+//   return (
+//     <div onClick={isDisabled ? undefined : onClick} className={classes} />
+//   );
+// }
 
-export const ItemSlider: React.FC<Props> = ({
-  name,
-}) => {
+export const ItemSlider: React.FC<Props> = ({ name }) => {
   const data = useAsyncValue() as ProductShorted[];
   const settings = {
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 4,
     className: "slider-style",
-    nextArrow: (
-      <SampleNextArrow
-        isDisabled={false}
-        onClick={() => {}}
-      />
-    ),
-    prevArrow: (
-      <SamplePrevArrow
-        isDisabled={false}
-        onClick={() => {}}
-      />
-    ),
+    margin: 16,
+    // nextArrow: (
+    //   <SampleNextArrow
+    //     isDisabled={false}
+    //     onClick={() => {}}
+    //   />
+    // ),
+    // prevArrow: (
+    //   <SamplePrevArrow
+    //     isDisabled={false}
+    //     onClick={() => {}}
+    //   />
+    // ),
     responsive: [
       {
         breakpoint: 1040,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
-        },
+          slidesToScroll: 3
+        }
       },
       {
         breakpoint: 870,
         settings: {
           slidesToShow: 2.5,
-          slidesToScroll: 2,
-        },
+          slidesToScroll: 2
+        }
       },
       {
         breakpoint: 700,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-        },
+          slidesToScroll: 2
+        }
       },
       {
         breakpoint: 530,
         settings: {
           slidesToShow: 1.5,
-          slidesToScroll: 1,
-        },
+          slidesToScroll: 1
+        }
       },
       {
         breakpoint: 360,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   return (
-    <div className={styles.sliderContainer}>
+    <div className={`${styles.sliderContainer} card_slider`}>
       <h2 className={styles.sliderTitle}>{name}</h2>
       <Slider {...settings}>
         {/*{phones.map(phone => <ItemCard phone={phone} key={phone.id} />)}*/}
-        {data.reverse().map(phone => <ItemCard phone={phone} key={phone.id} />)}
+        {data.reverse().map(phone => (
+          <ItemCard phone={phone} key={phone.id} />
+        ))}
       </Slider>
     </div>
   );
 };
 
-SamplePrevArrow.defaultProps = {
-  onClick: () => {},
-};
+// SamplePrevArrow.defaultProps = {
+//   onClick: () => {},
+// };
 
-SampleNextArrow.defaultProps = {
-  onClick: () => {},
-};
+// SampleNextArrow.defaultProps = {
+//   onClick: () => {},
+// };
