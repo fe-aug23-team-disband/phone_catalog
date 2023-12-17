070ef8e844bd513ddb4400b0429421ff2a6a3ddd
Serhii Mykhailivskyi <107731333+adioskv@users.noreply.github.com>
2023-12-17 19:30:44 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Outlet, useLocation } from "react-router-dom";
 import Header from "../widgets/Header/Header";
 import Footer from "../widgets/Footer/Footer";
 import styles from "../styles/main.module.scss";
+import Bredcrumbs from "../widgets/Bredcrumbs/Bredcrumbs";
 
 export const App = () => {
   const location = useLocation();
