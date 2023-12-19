7541c8aa1d353a6a29ee0b51ce55f0a05f3030be
Serhii Mykhailivskyi <sergeymih098123@gmail.com>
2023-12-19 23:18:01 +0200

    itemCard fix

--- src/entities/ItemCard/ItemCard.tsx
+++ src/entities/ItemCard/ItemCard.tsx
@@ -2,7 +2,7 @@ import styles from "./ItemCard.module.scss";
 import { FavoriteButton } from "../../shared/FavoriteButton/FavoriteButton";
 import { AddToCartButton } from "../../shared/AddToCartButton/AddToCartButton";
 import { ProductShorted } from "../../types/Product";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { scrollToTop } from "../../shared/ScrollToTop/ScrollToTop";
 import { getCategory } from "../../shared/utils/getCategory";
 
@@ -27,26 +27,26 @@ export const ItemCard = ({ phone }: { phone: ProductShorted }) => {
           alt={phone.name}
           className={styles.itemCard__Image}
         />
-      </Link>
 
-      <div className={styles.itemCard__Title}>
-        <div className={styles.itemCard__Title_Text}>{phone.name}</div>
-      </div>
-
-      <div className={styles.itemCard__Price}>
-        <div className={styles.itemCard__Price_Current}>
-          ${phone.priceDiscount}
+        <div className={styles.itemCard__Title}>
+          <div className={styles.itemCard__Title_Text}>{phone.name}</div>
         </div>
 
-        <div className={styles.itemCard__Price_Full}>${phone.priceRegular}</div>
-      </div>
+        <div className={styles.itemCard__Price}>
+          <div className={styles.itemCard__Price_Current}>
+            ${phone.priceDiscount}
+          </div>
 
-      <div className={styles.itemCard__Specs}>
-        <Specs category="Screen" value={phone.screen} />
-        <Specs category="Capacity" value={phone.capacity} />
-        <Specs category="RAM" value={phone.ram} />
-      </div>
+          <div className={styles.itemCard__Price_Full}>${phone.priceRegular}</div>
+        </div>
 
+        <div className={styles.itemCard__Specs}>
+          <Specs category="Screen" value={phone.screen} />
+          <Specs category="Capacity" value={phone.capacity} />
+          <Specs category="RAM" value={phone.ram} />
+        </div>
+
+      </Link>
       <div className={styles.itemCard__ButtonWrapper}>
         <AddToCartButton product={phone} />
 
