283b6e3e5b928c4168d280ea37d0c355855fa52d
Serhii Mykhailivskyi <sergeymih098123@gmail.com>
2023-12-22 01:38:52 +0200

    fixed cards on contacts

--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,16 +1,18 @@
-// import { Developer } from "../../types/Developer";
 import { DeveloperCard } from "../../entities/DeveloperCard/DeveloperCard";
 import { Developer } from "../../types/Developer";
 import developers from "./contacts.json";
+import styles from "./ContactsPage.module.scss";
 
 export const ContactsPage = () => {
   const devs = developers;
 
   return (
     <>
-      <h1>Our developers</h1>
-      {devs.map((developer: Developer) =>
-        <DeveloperCard key={developer.name} developer={developer} />)}
+      <h1 className={styles.heading}>Our developers</h1>
+      <div className={styles.cards__container}>
+        {devs.map((developer: Developer) =>
+          <DeveloperCard key={developer.name} developer={developer} />)}
+      </div>
     </>
   );
 };
